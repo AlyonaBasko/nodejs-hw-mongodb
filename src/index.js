dde9209351b2fefab43301553a26f1f9a8c75c02
alona <alenabask93@gmail.com>
2025-08-05 13:53:14 +0200

    mongo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import { config } from 'dotenv';
+config(); // зчитує .env
+
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+async function startApp() {
+  await initMongoConnection(); // 1️⃣ Спочатку підключення до MongoDB
+  setupServer();               // 2️⃣ Потім запуск сервера
+}
+
+startApp();
