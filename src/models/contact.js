9546c0b349e0796b8225dec4745e6196049e6620
alona <alenabask93@gmail.com>
2025-08-09 20:51:54 +0200

    update1

--- src/models/contact.js
+++ src/models/contact.js
@@ -2,10 +2,24 @@ import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String },
-    isFavourite: { type: Boolean, default: false },
+    name: {
+      type: String,
+      required: true
+    },
+
+    phoneNumber:{
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String
+    },
+    
+    isFavourite: {
+      type: Boolean,
+      default: false
+    },
     contactType: {
       type: String,
       enum: ['work', 'home', 'personal'],
