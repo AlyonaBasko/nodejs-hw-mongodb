11574d7fbbca46f4a41e77e05e64ca65ca622da4
alona <alenabask93@gmail.com>
2025-08-15 21:38:41 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import { config } from 'dotenv';
-config(); 
+config();
 
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 async function startApp() {
-  await initMongoConnection(); 
-  setupServer();               
+  try {
+    await initMongoConnection(); 
+    setupServer();
+  } catch (err) {
+    console.error("Failed to start app:", err);
+    process.exit(1);
+  }
 }
 
 startApp();
