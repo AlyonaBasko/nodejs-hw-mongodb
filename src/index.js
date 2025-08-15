98b3a69bd04aac2eb62dde8c12d7909615a562b6
alona <alenabask93@gmail.com>
2025-08-15 21:44:54 +0200

    fixed server.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
+import { initMongoConnection } from "./db/initMongoConnection.js";
+import { setupServer } from "./server.js";
 
 async function startApp() {
-  try {
-    await initMongoConnection(); 
-    setupServer();
-  } catch (err) {
-    console.error("Failed to start app:", err);
-    process.exit(1);
-  }
+  await initMongoConnection();
+  setupServer();
 }
 
 startApp();
--- src/server.js
+++ src/server.js
@@ -5,21 +5,22 @@ import contactsRouter from "./routes/contacts.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+function setupServer() {
+  const app = express();
+  const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(pino());
-app.use(express.json());
+  app.use(cors());
+  app.use(pino());
+  app.use(express.json());
 
-app.use("/contacts", contactsRouter);
+  app.use("/contacts", contactsRouter);
 
-// 404
-app.use(notFoundHandler);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export { setupServer };
