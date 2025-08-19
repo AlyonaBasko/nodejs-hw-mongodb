6dd1928abe06d2b451c3ccd395a3ef6dccaf6a7f
alona <alenabask93@gmail.com>
2025-08-19 21:31:47 +0200

    Step 3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,11 +6,15 @@ import {
   deleteContact,
 } from '../services/contacts.js';
 import createError from "http-errors";
+import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 
 // GET /contacts
 export const handleGetAllContacts = async (req, res, next) => {
   try {
-    const contacts = await getAllContacts();
+    const { page, perPage } = parsePaginationParams(req.query);
+
+    const contacts = await getAllContacts({ page, perPage });
+    
     res.status(200).json({
       status: 200,
       message: "Successfully found contacts!",
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,24 @@
 import Contact from '../models/contact.js';
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 
-export const getAllContacts = async () => {
-  return await Contact.find();
+export const getAllContacts = async ({ page, perPage }) => {
+  const skip = (page - 1) * perPage;
+  const limit = perPage;
+
+  const contactsQuery = Contact.find();
+
+  const totalItems = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(totalItems, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
