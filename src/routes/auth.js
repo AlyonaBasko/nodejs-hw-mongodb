c544641decfdf0b569a3b540a582004d43334f46
alona <alenabask93@gmail.com>
2025-08-31 21:30:44 +0200

    updete routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { validateBody } from "../middlewares/validateBody.js";
+import { authenticate } from "../middlewares/authenticate.js";
 import { registerUserSchema, loginUserSchema } from "../validation/authSchemas.js";
 import { handleRegister, handleLogin, handleRefresh, logoutController } from "../controllers/auth.js";
 
@@ -20,11 +21,13 @@ router.post(
 
 router.post(
   "/refresh",
+  authenticate,
   ctrlWrapper(handleRefresh)
 );
 
 router.post(
   "/logout",
+  authenticate,
   logoutController);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
+import authRouter from "./routes/auth.js";
 import contactsRouter from "./routes/contacts.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
@@ -15,6 +16,7 @@ function setupServer() {
   app.use(pino());
   app.use(express.json());
 
+  app.use("/auth", authRouter);
   app.use("/contacts", contactsRouter);
 
   app.use(notFoundHandler);
