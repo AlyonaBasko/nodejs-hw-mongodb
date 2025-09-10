b74d8ae1ec3a18f9bc61f25f75800c6a8409658f
alona <alenabask93@gmail.com>
2025-09-10 08:20:04 +0200

    authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 import createHttpError from "http-errors";
 import { UserCollection } from "../models/user.js";
+import { SessionCollection } from "../models/Session.js";
 
 const ACCESS_SECRET = process.env.ACCESS_SECRET || "access_secret_key";
 
 export const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       throw createHttpError(401, "No token provided");
     }
@@ -24,12 +24,18 @@ export const authenticate = async (req, res, next) => {
       throw createHttpError(401, "Invalid access token");
     }
 
+    // 🔹 Перевіряємо, що токен є у сесіях
+    const session = await SessionCollection.findOne({ accessToken: token });
+    if (!session) {
+      throw createHttpError(401, "Session not found or logged out");
+    }
+
     const user = await UserCollection.findById(payload.userId).select("-password");
     if (!user) {
       throw createHttpError(401, "User not found");
     }
 
-    req.user = user; 
+    req.user = user;
     next();
   } catch (err) {
     next(err);
